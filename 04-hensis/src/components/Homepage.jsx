d3ea6f715b92c3550c4fae1781e9bd5a415e48ec
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-12-01 13:23:56 -0500

    Avance-02-Proyecto

--- 04-hensis/src/components/Homepage.jsx
+++ 04-hensis/src/components/Homepage.jsx
@@ -3,10 +3,53 @@ import '../stylesheets/Homepage.css'
 export const Homepage = () => {
   return (
     <>
-    <h1></h1>
-    
-    
-    
+      <div className='contenedor-completo' style={{height:'500vh'}}>
+        <div className='seccion-01 bg-gradient-fondo text-white' style={{ width: '100%', height: '180vh', }}>
+          <div className='descripcion-general-comic' style={{width: '100%', padding: '4rem', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10%'}}>
+            <div className='textos-1' style={{width: '37%', }}> 
+              <h2 style={{fontFamily:'sans-serif', fontSize:'40px', fontWeight: 'bold'}}>Saldo Negativo</h2> 
+              <h2 style={{fontFamily:'sans-serif', fontSize:'40px', fontWeight: 'bold'}}>Comic Interactivo</h2> <br/>
+              <p style={{textAlign: 'justify'}}>Retrata un mundo donde el valor humano se mide
+                por el aporte económico mensual. Quienes no alcanzan la cuota son
+                excluidos del sistema y marcados como "ciudadanos en saldo
+                negativo"</p>
+            </div>
+            <div><img src="../public/teaser-profesional.png" alt="" /></div>
+          </div>
+          <div style={{height: '20vh', justifyItems: 'center', alignContent: 'center'}}><h1>DESCRIPCIÓN DE LOS CAPÍTULOS</h1></div>
+          <div className='seccion-02'>
+            
+          </div>
+        </div>
+        <div style={{display:'flex', padding:'8rem', gap: '10%',}}>
+          <div style={{width:'53%', alignContent: 'center'}}>
+            <h2 style={{fontFamily:'sans-serif', fontSize:'34px', fontWeight: 'bolder'}}>Una historia original de Hensis</h2> <br/>
+            <p style={{lineHeight:'25px'}}>"Porque contar historias no es solo entretener, es provocar algo más profundo."</p> <br/>
+            <p style={{lineHeight: '25px'}}>Saldo Negativo nace desde nuestra visión de crear narrativas con impacto. Sigue leyendo y adéntrate en este universo postapocalíptico creado por nosotras!</p>
+          </div>
+          <div style={{justifyItems:'center', alignContent: 'center'}}><img src="../logo-libro.png" alt="" /></div>
+        </div>
+        <div className='seccion-03'>
+          <div style={{justifyItems:'center', alignContent: 'center'}}>
+            <h2 style={{fontFamily:'sans-serif', fontSize:'48px', fontWeight: 'bold'}}>Tienda</h2> <br/> <br/>
+            <p>Nos tomamos el tiempo para crear con sentido. Aquí puedes ver un adelanto del proceso detrás de lo que pronto estará en tus manos.</p>
+          </div> <br/>
+          <div style={{padding: '2rem'}}>
+            <div style={{}}>
+              <img src="../public/gorra.jpeg" alt="" />
+            </div> <br />
+            <div style={{width:'100%', display: 'flex', justifyContent: 'center', gap:'30px'}}>
+              <img style={{width: '19%'}} src="../public/camisa-ojos.jpeg" alt="" />
+              <img style={{width: '19%'}} src="../public/camisa-alas.jpeg" alt="" />
+              <img style={{width: '19%'}} src="../public/sudadera.jpeg" alt="" />
+              <img style={{width: '19%'}} src="../public/bolso.jpeg" alt="" />
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </>
   )
 }
--- 04-hensis/src/components/Navbar.jsx
+++ 04-hensis/src/components/Navbar.jsx
@@ -3,26 +3,26 @@ import '../stylesheets/Navbar.css'
 export const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="../public/Logo.png" alt="" style={{width: '11%'}}/></a>
+      <nav class="navbar navbar-expand-lg bg-body-white">
+        <div class="container-fluid bg-white">
+        <a class="navbar-brand" href="#" style={{width: '34%'}}><img src="../public/Logo.png" alt="" style={{width: '22%'}}/></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav" style={{gap: '45px'}}>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">Nuestra Marca</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Comic</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="#">Tienda</a>
               </li>
-              <li class="nav-item">
+              {/* <li class="nav-item">
                 <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
