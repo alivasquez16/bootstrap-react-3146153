fb9fd0e712bf8447e331f422e7e5762d680db266
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-12-04 13:24:00 -0500

    Avance-03-Proyecto

--- 04-hensis/src/components/Homepage.jsx
+++ 04-hensis/src/components/Homepage.jsx
@@ -30,21 +30,27 @@ export const Homepage = () => {
           <div style={{justifyItems:'center', alignContent: 'center'}}><img src="../logo-libro.png" alt="" /></div>
         </div>
         <div className='seccion-03'>
-          <div style={{justifyItems:'center', alignContent: 'center'}}>
-            <h2 style={{fontFamily:'sans-serif', fontSize:'48px', fontWeight: 'bold'}}>Tienda</h2> <br/> <br/>
-            <p>Nos tomamos el tiempo para crear con sentido. Aquí puedes ver un adelanto del proceso detrás de lo que pronto estará en tus manos.</p>
-          </div> <br/>
-          <div style={{padding: '2rem'}}>
-            <div style={{}}>
-              <img src="../public/gorra.jpeg" alt="" />
-            </div> <br />
-            <div style={{width:'100%', display: 'flex', justifyContent: 'center', gap:'30px'}}>
+          <div style={{height:'40vh', justifyItems:'center', alignContent: 'center'}}>
+            <h2 style={{fontFamily:'sans-serif', fontSize:'48px', fontWeight: 'bold'}}>Tienda</h2> 
+            <p className='mt-3'>Nos tomamos el tiempo para crear con sentido. Aquí puedes ver un adelanto del proceso detrás de lo que pronto estará en tus manos.</p>
+          </div> 
+          <div style={{padding: '2rem',position: 'relative'}}>
+            <div style={{width: '100%', display: 'flex', justifyContent: 'space-around'}}>
+              <img className='m-h' style={{width:'8%', position:'absolute'}} src="../public/H verde.png" alt="" />
+              <img style={{width: '83%'}} src="../public/gorra.jpeg" alt="" />
+              <div className='bg-turquesa-oscuro m-t p-2' style={{width: '23%', height: '16vh', position: 'absolute'}}>
+                <p style={{color: 'white', fontSize: '17px', justifyContent: 'center'}}>Gorra negra con estampado del logo de Hensis</p>
+                <p style={{color: 'white', fontSize: '17px'}}>34.000 cop</p>
+              </div>
+              
+            </div> 
+            <div className='mt-3' style={{width:'100%', display: 'flex', justifyContent: 'center', gap:'30px'}}>
               <img style={{width: '19%'}} src="../public/camisa-ojos.jpeg" alt="" />
               <img style={{width: '19%'}} src="../public/camisa-alas.jpeg" alt="" />
               <img style={{width: '19%'}} src="../public/sudadera.jpeg" alt="" />
               <img style={{width: '19%'}} src="../public/bolso.jpeg" alt="" />
-
             </div>
+            <img className='mb-h' style={{position: 'absolute', width: '8%'}} src="../public/H roja.png" alt="" />
           </div>
         </div>
       </div>
